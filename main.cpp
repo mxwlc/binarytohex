47ec863247906ca7956ca9bc802489ba8c46564b
mxwlc <94302084+mxwlc@users.noreply.github.com>
2022-02-17 14:17:21 +0000

    Added a Hex to binary only works for 1 character atm

--- main.cpp
+++ main.cpp
@@ -30,6 +30,30 @@ string btoh(string number){
 	return table[number];
 }
 
+//hex to 4 bit binary
+string htob(string number){
+	map<string, string> table;
+	table["0"]="0000";
+	table["1"]="0001";
+	table["2"]="0010";
+	table["3"]="0011";
+	table["4"]="0100";
+	table["5"]="0101";
+	table["6"]="0110";
+	table["7"]="0111";
+	table["8"]="1000";
+	table["9"]="1001";
+	table["A"]="1010";
+	table["B"]="1011";
+	table["C"]="1100";
+	table["D"]="1101";
+	table["E"]="1110";
+	table["F"]="1111";
+	if(!table.count(number)){
+		return "Fail";
+	}
+	return table[number];
+}
 
 
 int main(){
